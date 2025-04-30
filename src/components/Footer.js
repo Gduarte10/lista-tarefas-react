8d9c0f0e51d6bfc98bbafec33810761fb31b2465
Gabriel duarte <duartegabriel0102@gmail.com>
2025-04-29 21:07:29 -0300

    ll

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 // Footer.jsx
 const Footer = () => {
-    return <footer className="mt-4 text-muted small">Feito com carinho em React</footer>;
+    return <footer className="mt-4 text-muted small">Prova Prática 1 - React: Gabriel</footer>;
 }
 
 export default Footer
